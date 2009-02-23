47e471916d8c77786adffdce775542e017864756
AK <captainalex@gmail.com>
2009-02-23 11:07:42 -0500

    Red Square

---
+++ MyCustomView.h
@@ -0,0 +1,24 @@
+//
+//  MyCustomView.h
+//  RedSquare
+//
+//  Created by AK on 2/22/08.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MyCustomView : UIView {
+	
+	CGFloat                    squareSize;
+	CGFloat                    rotation;
+	CGColorRef                 aColor;
+	BOOL                       twoFingers;
+	
+	IBOutlet UILabel           *xField;
+	IBOutlet UILabel           *yField;
+	IBOutlet UILabel           *zField;
+}
+
+@end
